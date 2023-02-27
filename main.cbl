1bc2c8e53287d0de882fd0d03e5a38604651383c
Finn <34718806+FinnDore@users.noreply.github.com>
2023-02-27 21:59:54 +0000

    ඞ

---
+++ main.cbl
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. no-yes-end-loop-very-sussy.
+
+       PROCEDURE DIVISION.
+       PERFORM UNTIL 1 < 0
+          DISPLAY "ඞ"
+          CONTINUE AFTER 30 SECONDS
+       END-PERFORM.
+       STOP RUN.
